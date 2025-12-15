ea3d2d5dd8243b3acc6b764c1ae1f333c84f3b71
TheInterceptorYT <132172096+TheInterceptorYT@users.noreply.github.com>
2025-12-14 21:17:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -219,3 +219,4 @@ document.getElementById("restartBtn").addEventListener("click", () => {
 
 // ---------------- INIT ---------------- //
 window.addEventListener("DOMContentLoaded", maybeSpawnWithered);
+
